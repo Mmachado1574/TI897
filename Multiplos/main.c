0c6d18eda12bc8f25b1c418cb280f0dbc49fed76
mmachado <marcomachado.note@gmail.com>
2024-09-20 12:04:48 +0100

    multiplos 2, 3 e 5

--- Multiplos/main.c
+++ Multiplos/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int v;
+    setlocale(LC_ALL, "Portuguese");
+    printf("Insira um valor inteiro\n");
+    scanf("%d",&v);
+    if (v%2==0)
+        printf ("%d é multiplo de 2\n",v);
+    if (v%3==0)
+        printf ("%d e multiplo de 3\n",v);
+    f (v%5==0)
+        printf ("%d e multiplo de 5\n",v);
     return 0;
 }
